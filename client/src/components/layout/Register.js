b9a8f2fa45bdba31520bdc342bd908caaf71acd9
Melody-Ruth <melody@sd-fun.org>
2020-08-26 12:54:12 -0700

    working on create user page

--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 import axios from "axios";
 import "../../App.css";
 
@@ -10,11 +9,15 @@ export default class CreateAccount extends Component {
     //Allows us to use this in these methods to refer to the class CreateExercises
     this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
+	this.onChangePasswordConfirm = this.onChangePasswordConfirm.bind(this);
+	this.onChangeName = this.onChangeName.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       email: "",
       password: "",
+	  passwordConfirm: "",
+	  name: ""
     };
   }
 
@@ -22,33 +25,100 @@ export default class CreateAccount extends Component {
     this.setState({
       email: e.target.value,
     });
+    var emailProblemText = document.getElementById("emailProblem");
+	emailProblemText.innerHTML = "";
   }
 
   onChangePassword(e) {
     this.setState({
       password: e.target.value,
     });
+    var pwProblemText = document.getElementById("pwProblem");
+	pwProblemText.innerHTML = "";
+  }
+
+  onChangePasswordConfirm(e) {
+    this.setState({
+      passwordConfirm: e.target.value,
+    });
+    var pwProblemText = document.getElementById("pwConfirmProblem");
+	pwProblemText.innerHTML = "";
+  }
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value,
+    });
   }
 
   onSubmit(e) {
     e.preventDefault(); //prevents default html form submit behavior
 
-    const user = {
-      email: this.state.email,
-      password: this.state.password,
-    };
+	var okInput = true;
 
-    this.setState({
-      email: "",
-      password: "",
-    });
-
-    console.log(user);
-
-    //Send post request to the database. Replaces the postman stuff
-    axios
-      .post("http://localhost:5000/", user)
-      .then((res) => console.log(res.data));
+	if (this.state.email.indexOf('@') <= 0 || this.state.email.indexOf('@') >= this.state.email.length - 1) {
+		okInput = false;
+		var emailProblemText = document.getElementById("emailProblem");
+		emailProblemText.innerHTML = "Please enter a valid email.";
+	}
+	
+	var pwOk = true;
+	var currPW = this.state.password;
+	var capLetter = false;
+	var lowerLetter = false;
+	var number = false;
+	
+	if (currPW < 8) {
+		pwOk = false;
+	} else {
+		for (var i = 0; i < currPW.length; i++) {
+			if (currPW.charAt(i) >= 'A' && currPW.charAt(i) <= 'Z') {
+				capLetter = true;
+			}
+			if (currPW.charAt(i) >= 'a' && currPW.charAt(i) <= 'z') {
+				lowerLetter = true;
+			}
+			if (currPW.charAt(i) >= '0' && currPW.charAt(i) <= '9') {
+				number = true;
+			}
+		}
+		if (!capLetter || !lowerLetter || !number) {
+			pwOk = false;
+		}
+	}
+	
+	if (!pwOk) {
+		okInput = false;
+		var pwProblemText = document.getElementById("pwProblem");
+		pwProblemText.innerHTML = "Please enter a valid password. A valid password is over 8 characters long and has at least one capital letter, at least one lower case letter, and at least one number.";
+	}
+	
+	if (this.state.password != this.state.passwordConfirm) {
+		okInput = false;
+		var pwProblemText = document.getElementById("pwConfirmProblem");
+		pwProblemText.innerHTML = "Please enter the same password for both fields.";
+	}
+	
+	if (okInput) {
+		const user = {
+	      email: this.state.email,
+	      password: this.state.password,
+		  name: this.state.name
+	    };
+	
+	    this.setState({
+	      email: "",
+	      password: "",
+		  passwordConfirm: "",
+		  name: ""
+	    });
+	
+	    console.log(user);
+	
+	    //Send post request to the database
+	    
+	}
+    
   }
 
   render() {
@@ -61,33 +131,51 @@ export default class CreateAccount extends Component {
         </div>
         <div id="formDiv">
           <form onSubmit={this.onSubmit} id="form">
+			<h2 className="text" id="headerText">Create Account</h2>
             <div className="form-group" id="formInput">
-              <div id="labels">
-                <label className="spaceBelow text">Email: </label>
-                <label className="spaceBelow text">Password: </label>
-              </div>
-              <div id="inputs">
-                <input
-                  type="text"
-                  required
-                  className="form-control"
-                  value={this.state.email}
-                  onChange={this.onChangeEmail}
+				<input
+	              type="text"
+	              required
+	              className="form-control"
+	              value={this.state.name}
+				  placeholder="Name"
+                  onChange={this.onChangeName}
                   className="spaceBelow input"
                 />
                 <input
-                  type="text"
+	              type="text"
+	              required
+	              className="form-control"
+	              value={this.state.email}
+				  placeholder="Email"
+                  onChange={this.onChangeEmail}
+                  className="input"
+                />
+				<p class="problemText" id="emailProblem"></p>
+                <input
+                  type="password"
                   required
                   className="form-control"
                   value={this.state.password}
                   onChange={this.onChangePassword}
-                  className="spaceBelow input"
+                  className="input"
+				  placeholder="Password"
                 />
-              </div>
+				<p class="problemText" id="pwProblem"></p>
+ 				<input
+                  type="password"
+                  required
+                  className="form-control"
+                  value={this.state.passwordConfirm}
+                  onChange={this.onChangePasswordConfirm}
+                  className="input"
+				  placeholder="Confirm Password"
+                />
+				<p class="problemText" id="pwConfirmProblem"></p>
             </div>
             <div className="form-group">
               <button type="submit" className="myButton">
-                Create Account
+                Register
               </button>
             </div>
           </form>
