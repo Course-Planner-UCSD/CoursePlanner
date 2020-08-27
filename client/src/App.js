f791ef5af554ab2624b26f7588ad0226a81fb3b1
Melody-Ruth <melody@sd-fun.org>
2020-08-27 14:08:50 -0700

    fixing file path

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Fragment } from "react";
 
 import "./App.css";
 
-import CreateAccount from "./components/layout/Register";
-import MyNavBar from "./components/layout/navBar";
+import CreateAccount from "./components/auth/Register";
+import MyNavBar from "./components/auth/navBar";
 
 
 function App() {
