0548e8bc2973db79672e1516d3095340c985b17a
Advay Sharma <sharmaadvay0@gmail.com>
2020-08-27 14:30:47 -0700

    updating backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,17 @@ const config = require("config");
 //checks if token is valid and gets the user id if it is
 module.exports = function (req, res, next) {
   // Get token from header
-  const token = req.header("x-auth-token");
+  const userToken = req.header("x-auth-token");
 
   // check if no token
-  if (!token) {
+  if (!userToken) {
     return res.status(401).json({ msg: "Authorization Denied" });
   }
 
   try {
     //decoding the token and setting the request user equal to the decoded user
     //the object only contains the id which will can be used to get all user data
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(userToken, config.get("jwtSecret"));
 
     req.user = decoded.user;
 
