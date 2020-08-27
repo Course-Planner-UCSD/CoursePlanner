dbf0fc1d5a5b121490cb99fb4e6653e5aa39661d
Melody-Ruth <melody@sd-fun.org>
2020-08-27 14:04:48 -0700

    updating to use material ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment } from "react";
 
 import "./App.css";
 import CreateAccount from "./components/layout/Register";
+import MyNavBar from "./components/layout/navBar";
 function App() {
   return (
     <Fragment>
-      <CreateAccount />
+	  <MyNavBar/>
+      <CreateAccount/>
     </Fragment>
   );
 }
---
+++ client/src/myTheme.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const myTheme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      dark: '#5ba1e6',
+      main: '#69b2fa'
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#f7ce5c',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  spacing: 1,
+});
+
+export default myTheme;
