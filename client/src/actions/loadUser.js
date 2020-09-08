0f4c5fd73dae686510e6439a174c8cfd4fed3970
Advay Sharma <sharmaadvay0@gmail.com>
2020-09-08 14:32:31 -0700

    cleaning token system

--- client/src/actions/loadUser.js
+++ client/src/actions/loadUser.js
@@ -1,16 +1,20 @@
 import axios from "axios";
-import setToken from "../other/setToken";
 import { LOADED_USER, AUTH_ERROR } from "../other/types";
 
 export const loadUserData = () => async (dispatch) => {
   //set the send token to the local storage token
+  var config = {};
   if (localStorage.token) {
-    setToken(localStorage.token);
+    config = {
+      headers: {
+        "x-auth-token": localStorage.token,
+      },
+    };
   }
 
   //gets the user data and dispatches it to the state
   await axios
-    .get("api/userData")
+    .get("api/userData", config)
     .then((response) =>
       dispatch({
         type: LOADED_USER,
