34c71fa4d753ae4b0072423ad754ccc21fc8b850
Advay Sharma <sharmaadvay0@gmail.com>
2020-09-10 15:06:09 -0700

    improved performance by breaking up page.js

---
+++ client/src/components/layout/QuarterTable.js
@@ -0,0 +1,206 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import MaterialTable from "material-table";
+import axios from "axios";
+import { updatePlan } from "../../Redux/actions/plan";
+import moment from "moment";
+
+const QuarterTable = ({
+  token,
+  planData,
+  updatePlan,
+  planID,
+  planIndex,
+  year,
+  quarterNum,
+}) => {
+  const [data, setData] = useState({
+    columns: [
+      { title: "Courses", field: "course" },
+      { title: "Units", field: "units" },
+    ],
+    planIndex: null,
+    tableInfo: [],
+    title: "",
+  });
+
+  useEffect(() => {
+    initialState();
+  }, [planIndex, planData]);
+
+  const initialState = () => {
+    var tableInfo;
+    var title;
+    if (year === "firstYear") {
+      tableInfo = planData[planIndex].firstYear.quarters[quarterNum].courses;
+      title = planData[planIndex].firstYear.quarters[quarterNum].season;
+    } else if (year === "secondYear") {
+      tableInfo = planData[planIndex].secondYear.quarters[quarterNum].courses;
+      title = planData[planIndex].secondYear.quarters[quarterNum].season;
+    } else if (year === "thirdYear") {
+      tableInfo = planData[planIndex].thirdYear.quarters[quarterNum].courses;
+      title = planData[planIndex].thirdYear.quarters[quarterNum].season;
+    } else if (year === "fourthYear") {
+      tableInfo = planData[planIndex].fourthYear.quarters[quarterNum].courses;
+      title = planData[planIndex].fourthYear.quarters[quarterNum].season;
+    } else if (year === "fifthYear") {
+      tableInfo = planData[planIndex].fifthYear.quarters[quarterNum].courses;
+      title = planData[planIndex].fifthYear.quarters[quarterNum].season;
+    }
+    setData({
+      ...data,
+      planIndex,
+      tableInfo,
+      title,
+    });
+  };
+
+  const updateTableCellEdit = async (
+    newValue,
+    rowData,
+    columnDef,
+    year,
+    quarterNum
+  ) => {
+    const courseNum = rowData.tableData.id;
+
+    const config = {
+      headers: {
+        "x-auth-token": token,
+        "Content-Type": "application/json",
+      },
+    };
+    var currentPlanData;
+
+    if (year === "firstYear") {
+      currentPlanData = planData[data.planIndex].firstYear;
+    }
+    if (year === "secondYear") {
+      currentPlanData = planData[data.planIndex].secondYear;
+    }
+    if (year === "thirdYear") {
+      currentPlanData = planData[data.planIndex].thirdYear;
+    }
+    if (year === "fourthYear") {
+      currentPlanData = planData[data.planIndex].fourthYear;
+    }
+    if (year === "fifthYear") {
+      currentPlanData = planData[data.planIndex].fifthYear;
+    }
+
+    if (columnDef.field === "course") {
+      if (newValue === "") {
+        currentPlanData.quarters[quarterNum].courses[courseNum].course = "-";
+      } else {
+        currentPlanData.quarters[quarterNum].courses[
+          courseNum
+        ].course = newValue;
+      }
+    }
+
+    if (columnDef.field === "units") {
+      if (newValue === "") {
+        currentPlanData.quarters[quarterNum].courses[courseNum].units = "-";
+      } else {
+        currentPlanData.quarters[quarterNum].courses[
+          courseNum
+        ].units = newValue;
+      }
+    }
+
+    var currentTime = moment().toISOString();
+    var body;
+    if (year === "firstYear") {
+      body = JSON.stringify({
+        firstYear: currentPlanData,
+        modifiedDate: currentTime,
+      });
+    }
+    if (year === "secondYear") {
+      body = JSON.stringify({
+        secondYear: currentPlanData,
+        modifiedDate: currentTime,
+      });
+    }
+    if (year === "thirdYear") {
+      body = JSON.stringify({
+        thirdYear: currentPlanData,
+        modifiedDate: currentTime,
+      });
+    }
+    if (year === "fourthYear") {
+      body = JSON.stringify({
+        fourthYear: currentPlanData,
+        modifiedDate: currentTime,
+      });
+    }
+    if (year === "fifthYear") {
+      body = JSON.stringify({
+        fifthYear: currentPlanData,
+        modifiedDate: currentTime,
+      });
+    }
+
+    var url = "/api/coursePlan/updatePlan/" + planID;
+    await axios
+      .post(url, body, config)
+      .catch((err) => console.error(err))
+      .then((result) => {
+        updatePlan(result.data, planData, data.planIndex);
+      });
+    setData({
+      ...data,
+      tableInfo: currentPlanData.quarters[quarterNum].courses,
+    });
+  };
+
+  return (
+    <MaterialTable
+      title={data.title}
+      columns={data.columns}
+      data={data.tableInfo}
+      cellEditable={{
+        onCellEditApproved: (newValue, oldValue, rowData, columnDef) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              updateTableCellEdit(
+                newValue,
+                rowData,
+                columnDef,
+                year,
+                quarterNum
+              );
+              resolve();
+            }, 1000);
+          }),
+      }}
+      options={{
+        search: false,
+        tableLayout: "auto",
+        draggable: false,
+        headerStyle: {
+          fontSize: 20,
+        },
+        rowStyle: {
+          fontSize: 18,
+        },
+        paging: false,
+        padding: "dense",
+        paginationType: "normal",
+      }}
+    />
+  );
+};
+
+QuarterTable.propTypes = {
+  token: PropTypes.string,
+  updatePlan: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  token: state.authReducer.token,
+  planData: state.planReducer.planData,
+});
+
+export default connect(mapStateToProps, { updatePlan })(QuarterTable);
--- client/src/components/pages/Plan.js
+++ client/src/components/pages/Plan.js
@@ -2,28 +2,18 @@ import React, { Fragment, useState, useLayoutEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Redirect, useParams } from "react-router-dom";
-import MaterialTable from "material-table";
-import axios from "axios";
 import { updatePlan } from "../../Redux/actions/plan";
-import moment from "moment";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import Button from "@material-ui/core/Button";
 import { ThemeProvider } from "@material-ui/styles";
 import myTheme from "../layout/myTheme.component";
-import Card from "@material-ui/core/Card";
+import QuarterTable from "../layout/QuarterTable";
+import Notes from "../layout/Notes";
+import ModifiedDate from "../layout/ModifiedDate";
 
-const Plan = ({ userAuth, token, planData, updatePlan }) => {
+const Plan = ({ userAuth, planData }) => {
   let { planID } = useParams();
 
   const [data, setData] = useState({
-    columns: [
-      { title: "Courses", field: "course" },
-      { title: "Units", field: "units" },
-    ],
-    lastModified: null,
     planIndex: null,
-    text: "",
   });
 
   useLayoutEffect(() => {
@@ -50,121 +40,7 @@ const Plan = ({ userAuth, token, planData, updatePlan }) => {
     setData({
       ...data,
       planIndex: finalIndex,
-      text: planData[finalIndex].notes,
-      lastModified: moment(planData[finalIndex].modifiedDate).format(
-        "MMMM Do, h:mm a"
-      ),
     });
-  };
-
-  const updateTableCellEdit = async (
-    newValue,
-    rowData,
-    columnDef,
-    year,
-    quarterNum
-  ) => {
-    const courseNum = rowData.tableData.id;
-
-    const config = {
-      headers: {
-        "x-auth-token": token,
-        "Content-Type": "application/json",
-      },
-    };
-    var currentPlanData;
-
-    if (year === "firstYear") {
-      currentPlanData = planData[data.planIndex].firstYear;
-    }
-    if (year === "secondYear") {
-      currentPlanData = planData[data.planIndex].secondYear;
-    }
-    if (year === "thirdYear") {
-      currentPlanData = planData[data.planIndex].thirdYear;
-    }
-    if (year === "fourthYear") {
-      currentPlanData = planData[data.planIndex].fourthYear;
-    }
-    if (year === "fifthYear") {
-      currentPlanData = planData[data.planIndex].fifthYear;
-    }
-
-    if (columnDef.field === "course") {
-      currentPlanData.quarters[quarterNum].courses[courseNum].course = newValue;
-    }
-
-    if (columnDef.field === "units") {
-      currentPlanData.quarters[quarterNum].courses[courseNum].units = newValue;
-    }
-
-    var currentTime = moment().toISOString();
-    var body;
-    if (year === "firstYear") {
-      body = JSON.stringify({
-        firstYear: currentPlanData,
-        modifiedDate: currentTime,
-      });
-    }
-    if (year === "secondYear") {
-      body = JSON.stringify({
-        secondYear: currentPlanData,
-        modifiedDate: currentTime,
-      });
-    }
-    if (year === "thirdYear") {
-      body = JSON.stringify({
-        thirdYear: currentPlanData,
-        modifiedDate: currentTime,
-      });
-    }
-    if (year === "fourthYear") {
-      body = JSON.stringify({
-        fourthYear: currentPlanData,
-        modifiedDate: currentTime,
-      });
-    }
-    if (year === "fifthYear") {
-      body = JSON.stringify({
-        fifthYear: currentPlanData,
-        modifiedDate: currentTime,
-      });
-    }
-
-    var url = "/api/coursePlan/updatePlan/" + planID;
-    await axios
-      .post(url, body, config)
-      .catch((err) => console.error(err))
-      .then((result) => {
-        updatePlan(result.data, planData, data.planIndex);
-      });
-    var newModified = moment(planData[data.planIndex].modifiedDate).format(
-      "MMMM Do, h:mm a"
-    );
-    setData({ ...data, lastModified: newModified });
-  };
-
-  const textboxChange = (value) => {
-    setData({ ...data, text: value });
-  };
-
-  const saveNotes = async () => {
-    var url = "/api/coursePlan/updatePlan/" + planID;
-    const config = {
-      headers: {
-        "x-auth-token": token,
-        "Content-Type": "application/json",
-      },
-    };
-
-    var body = JSON.stringify({ notes: data.text });
-
-    await axios
-      .post(url, body, config)
-      .catch((err) => console.error(err))
-      .then((result) => {
-        updatePlan(result.data, planData, data.planIndex);
-      });
   };
 
   if (!userAuth) {
@@ -177,194 +53,77 @@ const Plan = ({ userAuth, token, planData, updatePlan }) => {
         <Fragment>
           {data.planIndex != null ? (
             <Fragment>
+              {console.log("render test")}
               <div className="planHeader">
                 <h1>{planData[data.planIndex].name}</h1>
-                <h3>
-                  Modified On:
-                  {" " + data.lastModified}
-                </h3>
+                <ModifiedDate planIndex={data.planIndex} />
               </div>
               <div className="planLeft">
-                <MaterialTable
-                  title="Fall"
-                  columns={data.columns}
-                  data={planData[data.planIndex].firstYear.quarters[0].courses}
-                  cellEditable={{
-                    onCellEditApproved: (
-                      newValue,
-                      oldValue,
-                      rowData,
-                      columnDef
-                    ) =>
-                      new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                          updateTableCellEdit(
-                            newValue,
-                            rowData,
-                            columnDef,
-                            "firstYear",
-                            0
-                          );
-                          resolve();
-                        }, 3000);
-                      }),
-                  }}
-                  options={{
-                    search: false,
-                    tableLayout: "auto",
-                    draggable: false,
-                    headerStyle: {
-                      fontSize: 20,
-                    },
-                    rowStyle: {
-                      fontSize: 18,
-                    },
-                    paging: false,
-                    padding: "dense",
-                    paginationType: "normal",
-                  }}
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"firstYear"}
+                  quarterNum={0}
                 />
               </div>
               <div className="plan">
-                <MaterialTable
-                  title="Winter"
-                  columns={data.columns}
-                  data={planData[data.planIndex].firstYear.quarters[1].courses}
-                  cellEditable={{
-                    onCellEditApproved: (
-                      newValue,
-                      oldValue,
-                      rowData,
-                      columnDef
-                    ) =>
-                      new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                          updateTableCellEdit(
-                            newValue,
-                            rowData,
-                            columnDef,
-                            "firstYear",
-                            1
-                          );
-                          resolve();
-                        }, 3000);
-                      }),
-                  }}
-                  options={{
-                    search: false,
-                    tableLayout: "auto",
-                    draggable: false,
-                    headerStyle: {
-                      fontSize: 20,
-                    },
-                    rowStyle: {
-                      fontSize: 18,
-                    },
-                    paging: false,
-                    padding: "dense",
-                    paginationType: "normal",
-                  }}
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"firstYear"}
+                  quarterNum={1}
                 />
               </div>
               <div className="plan">
-                <MaterialTable
-                  title="Spring"
-                  columns={data.columns}
-                  data={planData[data.planIndex].firstYear.quarters[2].courses}
-                  cellEditable={{
-                    onCellEditApproved: (
-                      newValue,
-                      oldValue,
-                      rowData,
-                      columnDef
-                    ) =>
-                      new Promise((resolve, reject) => {
-                        updateTableCellEdit(
-                          newValue,
-                          rowData,
-                          columnDef,
-                          "firstYear",
-                          2
-                        );
-                        setTimeout(resolve(), 3000);
-                      }),
-                  }}
-                  options={{
-                    search: false,
-                    tableLayout: "auto",
-                    draggable: false,
-                    headerStyle: {
-                      fontSize: 20,
-                    },
-                    rowStyle: {
-                      fontSize: 18,
-                    },
-                    paging: false,
-                    padding: "dense",
-                    paginationType: "normal",
-                  }}
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"firstYear"}
+                  quarterNum={2}
                 />
               </div>
               <div className="plan">
-                <MaterialTable
-                  title="Summer"
-                  columns={data.columns}
-                  data={planData[data.planIndex].firstYear.quarters[3].courses}
-                  cellEditable={{
-                    onCellEditApproved: (
-                      newValue,
-                      oldValue,
-                      rowData,
-                      columnDef
-                    ) =>
-                      new Promise((resolve, reject) => {
-                        updateTableCellEdit(
-                          newValue,
-                          rowData,
-                          columnDef,
-                          "firstYear",
-                          3
-                        );
-                        setTimeout(resolve(), 3000);
-                      }),
-                  }}
-                  options={{
-                    search: false,
-                    tableLayout: "auto",
-                    draggable: false,
-                    headerStyle: {
-                      fontSize: 20,
-                    },
-                    rowStyle: {
-                      fontSize: 18,
-                    },
-                    paging: false,
-                    padding: "dense",
-                    paginationType: "normal",
-                  }}
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"firstYear"}
+                  quarterNum={3}
                 />
               </div>
-              <div>
-                <Card id="notes">
-                  <h2 className="text" id="notesText">
-                    Notes
-                  </h2>
-                  <ReactQuill
-                    value={data.text}
-                    className="textbox"
-                    onChange={textboxChange}
-                  />
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    className="save"
-                    onClick={saveNotes}
-                    id="saveNotesButton"
-                  >
-                    Save Notes
-                  </Button>
-                </Card>
+              <div className="planLeft">
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"secondYear"}
+                  quarterNum={0}
+                />
               </div>
+              <div className="plan">
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"secondYear"}
+                  quarterNum={1}
+                />
+              </div>
+              <div className="plan">
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"secondYear"}
+                  quarterNum={2}
+                />
+              </div>
+              <div className="plan">
+                <QuarterTable
+                  planID={planID}
+                  planIndex={data.planIndex}
+                  year={"secondYear"}
+                  quarterNum={3}
+                />
+              </div>
+
+              <Notes planID={planID} planIndex={data.planIndex} />
             </Fragment>
           ) : (
             <Fragment></Fragment>
@@ -377,12 +136,10 @@ const Plan = ({ userAuth, token, planData, updatePlan }) => {
 Plan.propTypes = {
   token: PropTypes.string,
   userAuth: PropTypes.bool,
-  updatePlan: PropTypes.func.isRequired,
 };
 const mapStateToProps = (state) => ({
   userAuth: state.authReducer.userAuth,
-  token: state.authReducer.token,
   planData: state.planReducer.planData,
 });
 
-export default connect(mapStateToProps, { updatePlan })(Plan);
+export default connect(mapStateToProps)(Plan);
