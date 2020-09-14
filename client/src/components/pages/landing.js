c76c4f195228cee234052922b688431d852c3483
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-09-13 17:03:52 -0700

    materialize a bit?

--- client/src/components/pages/landing.js
+++ client/src/components/pages/landing.js
@@ -3,6 +3,26 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import { ThemeProvider } from "@material-ui/styles";
+import myTheme from "../layout/myTheme.component";
+import Box from "@material-ui/core/Box";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Alert from "@material-ui/lab/Alert";
+import "../../materialize.css";
+import "../../materialize.min.css";
+import "../../style.css";
+
 const Landing = ({ userAuth }) => {
   if (userAuth) {
     return <Redirect to="/dashboard" />;
